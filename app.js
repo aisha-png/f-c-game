9aad733cc9136e57ff3960af35384c801ddae540
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-10-12 14:12:46 +0100

    fixed reload button

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const emojis = ["👾","👾","🤠","🤠","😈","😈","🤡","🤡","🤖","🤖","👺","👺","💅","💅","💀","💀"];
-var shuffle_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+var shuffle_emojis = emojis.slice().sort(() => (Math.random() > .5) ? 2 : -1);
 
 for (var i = 0; i < emojis.length; i++){
     let box = document.createElement('div');
